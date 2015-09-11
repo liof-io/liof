ff6b76d76b624cdf4df351481295b7f3583ad3e0
Vitaliy <vit@tag.ua>
2015-09-11 15:15:35 +0300

    Enable CORS

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,10 @@ module Liof
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => 'POST, GET, PUT, DELETE'
+    })
   end
 end
