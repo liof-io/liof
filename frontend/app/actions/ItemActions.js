e83edb3542579a356fb3ed2ea33b9b5b490e3fdc
Vitaliy <vit@tag.ua>
2015-09-16 16:59:11 +0300

    Use isomorphic-fetch, add async action

--- frontend/app/actions/ItemActions.js
+++ frontend/app/actions/ItemActions.js
@@ -1,26 +1,41 @@
-import request from 'axios';
+import fetch from 'isomorphic-fetch';
 
-export const RECEIVE_ITEMS  = 'RECEIVE_ITEMS';
 export const REQUEST_ITEMS  = 'REQUEST_ITEMS';
+export const RECEIVE_ITEMS  = 'RECEIVE_ITEMS';
 export const CREATE_ITEM    = 'CREATE_ITEM';
 export const EDIT_ITEM      = 'EDIT_ITEM';
 export const DELETE_ITEM    = 'DELETE_ITEM';
 export const COMPLETE_ITEM  = 'COMPLETE_ITEM';
 
-const API_URL = 'https://localhost:3000/lists';
-
-export function receiveItems() {
-  return {
-    type: RECEIVE_ITEMS,
-    promise: request.get(API_URL)
-  }
-}
+const API_URL = 'http://localhost:3000/lists.json';
 
 export function createItem(text) {
   return {
     type: CREATE_ITEM,
-    promise: request.post(API_URL, { text })
-  };
+    text
+  }
+}
+
+function requestItems() {
+  return {
+    type: REQUEST_ITEMS
+  }
+}
+
+function receiveItems(json) {
+  return {
+    type: RECEIVE_ITEMS,
+    items: json
+  }
+}
+
+export function fetchItems() {
+  return dispatch => {
+    dispatch(requestItems());
+    return fetch(API_URL)
+      .then(response => response.json())
+      .then(json => dispatch(receiveItems(json)));
+  }
 }
 
 export function editItem(id, text) {
